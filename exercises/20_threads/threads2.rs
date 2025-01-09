814080f51d6ca03bb6e76857df3d3ed042439f89
wavvXD! <2300864263@qq.com>
2025-01-09 16:44:45 +0800

    update

--- exercises/20_threads/threads2.rs
+++ exercises/20_threads/threads2.rs
@@ -2,33 +2,40 @@
 // work. But this time, the spawned threads need to be in charge of updating a
 // shared value: `JobStatus.jobs_done`
 
-use std::{sync::Arc, thread, time::Duration};
+use std::{
+    sync::{Arc, Mutex},
+    thread,
+    time::Duration,
+};
 
 struct JobStatus {
     jobs_done: u32,
 }
 
 fn main() {
-    // TODO: `Arc` isn't enough if you want a **mutable** shared state.
-    let status = Arc::new(JobStatus { jobs_done: 0 });
+    // 使用 `Arc` 和 `Mutex` 包裹 `JobStatus`，以实现线程安全的共享可变状态
+    let status = Arc::new(Mutex::new(JobStatus { jobs_done: 0 }));
 
     let mut handles = Vec::new();
     for _ in 0..10 {
+        // 克隆 `Arc`，以便每个线程可以共享所有权
         let status_shared = Arc::clone(&status);
         let handle = thread::spawn(move || {
             thread::sleep(Duration::from_millis(250));
 
-            // TODO: You must take an action before you update a shared value.
-            status_shared.jobs_done += 1;
+            // 获取 `Mutex` 的锁，以安全地修改共享数据
+            let mut status = status_shared.lock().unwrap();
+            status.jobs_done += 1;
         });
         handles.push(handle);
     }
 
-    // Waiting for all jobs to complete.
+    // 等待所有线程完成
     for handle in handles {
         handle.join().unwrap();
     }
 
-    // TODO: Print the value of `JobStatus.jobs_done`.
-    println!("Jobs done: {}", todo!());
+    // 打印最终的 `jobs_done` 值
+    let status = status.lock().unwrap();
+    println!("Jobs done: {}", status.jobs_done);
 }
--- exercises/21_macros/macros3.rs
+++ exercises/21_macros/macros3.rs
@@ -1,6 +1,10 @@
 // TODO: Fix the compiler error without taking the macro definition out of this
 // module.
+// 定义宏模块
+// 定义宏模块
 mod macros {
+    // 使用 #[macro_export] 将宏导出到 crate 的根作用域
+    #[macro_export]
     macro_rules! my_macro {
         () => {
             println!("Check out my macro!");
@@ -8,6 +12,9 @@ mod macros {
     }
 }
 
+
+
 fn main() {
+    // 使用宏
     my_macro!();
 }
