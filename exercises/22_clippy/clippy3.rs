000e021862a188d57b53de77081552e9f17eed85
wavvXD! <2300864263@qq.com>
2025-01-09 17:00:49 +0800

    update

--- exercises/22_clippy/clippy3.rs
+++ exercises/22_clippy/clippy3.rs
@@ -2,26 +2,28 @@
 // TODO: Fix all the Clippy lints.
 
 #[rustfmt::skip]
-#[allow(unused_variables, unused_assignments)]
+#[allow(unused_variables)] // 仍然允许未使用的变量，因为示例可能不完整
 fn main() {
     let my_option: Option<()> = None;
-    if my_option.is_none() {
-        println!("{:?}", my_option.unwrap());
+    if let Some(value) = my_option {
+        println!("{:?}", value);
+    } else {
+        println!("my_option is None");
     }
 
     let my_arr = &[
-        -1, -2, -3
-        -4, -5, -6
+        -1, -2, -3,
+        -4, -5, -6,
     ];
     println!("My array! Here it is: {my_arr:?}");
 
-    let my_empty_vec = vec![1, 2, 3, 4, 5].resize(0, 5);
-    println!("This Vec is empty, see? {my_empty_vec:?}");
+    let mut my_vec = vec![1, 2, 3, 4, 5];
+    my_vec.clear(); // 不需要填充值，因为我们是在清空Vec
+    println!("This Vec is empty, see? {:?}", my_vec);
 
     let mut value_a = 45;
     let mut value_b = 66;
     // Let's swap these two!
-    value_a = value_b;
-    value_b = value_a;
+    std::mem::swap(&mut value_a, &mut value_b);
     println!("value a: {value_a}; value b: {value_b}");
 }
